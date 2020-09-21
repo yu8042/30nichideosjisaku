3881292646b5b6775c21c748de9e516ee8972b46
Yu Yamashita <>
2020-09-21 12:11:12 +0900

    specialize in mac

--- 22_day/harib19g/a_nasm.asm
+++ 22_day/harib19g/a_nasm.asm
@@ -1,24 +1,21 @@
-[FORMAT "WCOFF"]
-[INSTRSET "i486p"]
-[BITS 32]
-[FILE "a_nask.nas"]
+[BITS 32]						; 32ビットモード用の機械語を作らせる
 
-		GLOBAL	_api_putchar
-		GLOBAL	_api_putstr0
-		GLOBAL	_api_end
-		GLOBAL	_api_openwin
-		GLOBAL	_api_putstrwin
-		GLOBAL	_api_boxfilwin
+		GLOBAL	api_putchar
+		GLOBAL	api_putstr0
+		GLOBAL	api_end
+		GLOBAL	api_openwin
+		GLOBAL	api_putstrwin
+		GLOBAL	api_boxfilwin
 
-[SECTION .text]
+SECTION .text
 
-_api_putchar:	; void api_putchar(int c);
+api_putchar:	; void api_putchar(int c);
 		MOV		EDX,1
 		MOV		AL,[ESP+4]		; c
 		INT		0x40
 		RET
 
-_api_putstr0:	; void api_putstr0(char *s);
+api_putstr0:	; void api_putstr0(char *s);
 		PUSH	EBX
 		MOV		EDX,2
 		MOV		EBX,[ESP+8]		; s
@@ -26,11 +23,11 @@ _api_putstr0:	; void api_putstr0(char *s);
 		POP		EBX
 		RET
 
-_api_end:	; void api_end(void);
+api_end:	; void api_end(void);
 		MOV		EDX,4
 		INT		0x40
 
-_api_openwin:	; int api_openwin(char *buf, int xsiz, int ysiz, int col_inv, char *title);
+api_openwin:	; int api_openwin(char *buf, int xsiz, int ysiz, int col_inv, char *title);
 		PUSH	EDI
 		PUSH	ESI
 		PUSH	EBX
@@ -46,7 +43,7 @@ _api_openwin:	; int api_openwin(char *buf, int xsiz, int ysiz, int col_inv, char
 		POP		EDI
 		RET
 
-_api_putstrwin:	; void api_putstrwin(int win, int x, int y, int col, int len, char *str);
+api_putstrwin:	; void api_putstrwin(int win, int x, int y, int col, int len, char *str);
 		PUSH	EDI
 		PUSH	ESI
 		PUSH	EBP
@@ -65,7 +62,7 @@ _api_putstrwin:	; void api_putstrwin(int win, int x, int y, int col, int len, ch
 		POP		EDI
 		RET
 
-_api_boxfilwin:	; void api_boxfilwin(int win, int x0, int y0, int x1, int y1, int col);
+api_boxfilwin:	; void api_boxfilwin(int win, int x0, int y0, int x1, int y1, int col);
 		PUSH	EDI
 		PUSH	ESI
 		PUSH	EBP

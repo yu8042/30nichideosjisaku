d1ec3948fbf24d7cd9d55c2fcee70666c5a05cac
Yu Yamashita <>
2020-09-21 17:38:54 +0900

    specialize in mac

--- 23_day/harib20e/a_nasm.asm
+++ 23_day/harib20e/a_nasm.asm
@@ -1,31 +1,28 @@
-[FORMAT "WCOFF"]
-[INSTRSET "i486p"]
-[BITS 32]
-[FILE "a_nask.nas"]
+[BITS 32]						; 32ビットモード用の機械語を作らせる
 
-		GLOBAL	_api_putchar
-		GLOBAL	_api_putstr0
-		GLOBAL	_api_end
-		GLOBAL	_api_openwin
-		GLOBAL	_api_putstrwin
-		GLOBAL	_api_boxfilwin
-		GLOBAL	_api_initmalloc
-		GLOBAL	_api_malloc
-		GLOBAL	_api_free
-		GLOBAL	_api_point
-		GLOBAL	_api_refreshwin
-		GLOBAL	_api_linewin
-		GLOBAL	_api_closewin
+		GLOBAL	api_putchar
+		GLOBAL	api_putstr0
+		GLOBAL	api_end
+		GLOBAL	api_openwin
+		GLOBAL	api_putstrwin
+		GLOBAL	api_boxfilwin
+		GLOBAL	api_initmalloc
+		GLOBAL	api_malloc
+		GLOBAL	api_free
+		GLOBAL	api_point
+		GLOBAL	api_refreshwin
+		GLOBAL	api_linewin
+		GLOBAL	api_closewin
 
-[SECTION .text]
+SECTION .text
 
-_api_putchar:	; void api_putchar(int c);
+api_putchar:	; void api_putchar(int c);
 		MOV		EDX,1
 		MOV		AL,[ESP+4]		; c
 		INT		0x40
 		RET
 
-_api_putstr0:	; void api_putstr0(char *s);
+api_putstr0:	; void api_putstr0(char *s);
 		PUSH	EBX
 		MOV		EDX,2
 		MOV		EBX,[ESP+8]		; s
@@ -33,11 +30,11 @@ _api_putstr0:	; void api_putstr0(char *s);
 		POP		EBX
 		RET
 
-_api_end:	; void api_end(void);
+api_end:	; void api_end(void);
 		MOV		EDX,4
 		INT		0x40
 
-_api_openwin:	; int api_openwin(char *buf, int xsiz, int ysiz, int col_inv, char *title);
+api_openwin:	; int api_openwin(char *buf, int xsiz, int ysiz, int col_inv, char *title);
 		PUSH	EDI
 		PUSH	ESI
 		PUSH	EBX
@@ -53,7 +50,7 @@ _api_openwin:	; int api_openwin(char *buf, int xsiz, int ysiz, int col_inv, char
 		POP		EDI
 		RET
 
-_api_putstrwin:	; void api_putstrwin(int win, int x, int y, int col, int len, char *str);
+api_putstrwin:	; void api_putstrwin(int win, int x, int y, int col, int len, char *str);
 		PUSH	EDI
 		PUSH	ESI
 		PUSH	EBP
@@ -72,7 +69,7 @@ _api_putstrwin:	; void api_putstrwin(int win, int x, int y, int col, int len, ch
 		POP		EDI
 		RET
 
-_api_boxfilwin:	; void api_boxfilwin(int win, int x0, int y0, int x1, int y1, int col);
+api_boxfilwin:	; void api_boxfilwin(int win, int x0, int y0, int x1, int y1, int col);
 		PUSH	EDI
 		PUSH	ESI
 		PUSH	EBP
@@ -91,19 +88,19 @@ _api_boxfilwin:	; void api_boxfilwin(int win, int x0, int y0, int x1, int y1, in
 		POP		EDI
 		RET
 
-_api_initmalloc:	; void api_initmalloc(void);
+api_initmalloc:	; void api_initmalloc(void);
 		PUSH	EBX
 		MOV		EDX,8
-		MOV		EBX,[CS:0x0020]		; malloc�̈�̔Ԓn
+		MOV		EBX,[CS:0x0020]		; malloc領域の番地
 		MOV		EAX,EBX
-		ADD		EAX,32*1024			; 32KB�𑫂�
-		MOV		ECX,[CS:0x0000]		; �f�[�^�Z�O�����g�̑傫��
+		ADD		EAX,32*1024			; 32KBを足す
+		MOV		ECX,[CS:0x0000]		; データセグメントの大きさ
 		SUB		ECX,EAX
 		INT		0x40
 		POP		EBX
 		RET
 
-_api_malloc:		; char *api_malloc(int size);
+api_malloc:		; char *api_malloc(int size);
 		PUSH	EBX
 		MOV		EDX,9
 		MOV		EBX,[CS:0x0020]
@@ -112,7 +109,7 @@ _api_malloc:		; char *api_malloc(int size);
 		POP		EBX
 		RET
 
-_api_free:			; void api_free(char *addr, int size);
+api_free:			; void api_free(char *addr, int size);
 		PUSH	EBX
 		MOV		EDX,10
 		MOV		EBX,[CS:0x0020]
@@ -122,7 +119,7 @@ _api_free:			; void api_free(char *addr, int size);
 		POP		EBX
 		RET
 
-_api_point:		; void api_point(int win, int x, int y, int col);
+api_point:		; void api_point(int win, int x, int y, int col);
 		PUSH	EDI
 		PUSH	ESI
 		PUSH	EBX
@@ -137,7 +134,7 @@ _api_point:		; void api_point(int win, int x, int y, int col);
 		POP		EDI
 		RET
 
-_api_refreshwin:	; void api_refreshwin(int win, int x0, int y0, int x1, int y1);
+api_refreshwin:	; void api_refreshwin(int win, int x0, int y0, int x1, int y1);
 		PUSH	EDI
 		PUSH	ESI
 		PUSH	EBX
@@ -153,7 +150,7 @@ _api_refreshwin:	; void api_refreshwin(int win, int x0, int y0, int x1, int y1);
 		POP		EDI
 		RET
 
-_api_linewin:		; void api_linewin(int win, int x0, int y0, int x1, int y1, int col);
+api_linewin:		; void api_linewin(int win, int x0, int y0, int x1, int y1, int col);
 		PUSH	EDI
 		PUSH	ESI
 		PUSH	EBP
@@ -172,7 +169,7 @@ _api_linewin:		; void api_linewin(int win, int x0, int y0, int x1, int y1, int c
 		POP		EDI
 		RET
 
-_api_closewin:		; void api_closewin(int win);
+api_closewin:		; void api_closewin(int win);
 		PUSH	EBX
 		MOV		EDX,14
 		MOV		EBX,[ESP+8]	; win

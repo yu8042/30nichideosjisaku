bde517096e38bf8b627ccd9c1ace452df938f0c0
Yu Yamashita <>
2020-10-01 00:45:38 +0900

    specialize in mac

--- 27_day/harib24a/hello5.asm
+++ 27_day/harib24a/hello5.asm
@@ -1,20 +1,17 @@
-[FORMAT "WCOFF"]
-[INSTRSET "i486p"]
 [BITS 32]
-[FILE "hello5.nas"]
 
-		GLOBAL	_HariMain
+		GLOBAL	HariMain
 
-[SECTION .text]
+SECTION .text
 
-_HariMain:
+HariMain:
 		MOV		EDX,2
 		MOV		EBX,msg
 		INT		0x40
 		MOV		EDX,4
 		INT		0x40
 
-[SECTION .data]
+SECTION .data
 
 msg:
 		DB	"hello, world", 0x0a, 0

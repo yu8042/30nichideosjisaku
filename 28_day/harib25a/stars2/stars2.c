46982f4686b15b1acea2ac2359b142adbcd27b00
Yu Yamashita <>
2020-10-08 22:58:55 +0900

    specialize in mac

--- 28_day/harib25a/stars2/stars2.c
+++ 28_day/harib25a/stars2/stars2.c
@@ -1,6 +1,14 @@
 #include "apilib.h"
 
-int rand(void);		/* 0`32767‚Ì”ÍˆÍ‚Å—”‚ğ”­¶ */
+unsigned long rand (void)
+{
+  static unsigned long rand;
+
+  rand *= 1234567;
+  rand += 1397;
+
+  return rand;
+}
 
 void HariMain(void)
 {
@@ -9,16 +17,16 @@ void HariMain(void)
 	api_initmalloc();
 	buf = api_malloc(150 * 100);
 	win = api_openwin(buf, 150, 100, -1, "stars2");
-	api_boxfilwin(win + 1,  6, 26, 143, 93, 0 /* • */);
+	api_boxfilwin(win + 1,  6, 26, 143, 93, 0 /* ï¿½ï¿½ */);
 	for (i = 0; i < 50; i++) {
 		x = (rand() % 137) +  6;
 		y = (rand() %  67) + 26;
-		api_point(win + 1, x, y, 3 /* ‰© */);
+		api_point(win + 1, x, y, 3 /* ï¿½ï¿½ */);
 	}
 	api_refreshwin(win,  6, 26, 144, 94);
 	for (;;) {
 		if (api_getkey(1) == 0x0a) {
-			break; /* Enter‚È‚çbreak; */
+			break; /* Enterãªã‚‰break; */
 		}
 	}
 	api_end();
